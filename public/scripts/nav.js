7c46669b1128fffc102343bb84a6cb2b3c5bf400
Matt Leuer <mattleuer@gmail.com>
2020-05-22 17:14:30 -0700

    Delete nav.js

--- public/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.navbar-nav .nav-link').click(function (event) {
-        event.stopPropagation();
-        console.log(event.currentTarget + "//" + event.target + "//" + this);
-        $('.navbar-nav .nav-link').removeClass('active');
-        $(this).addClass('active');
-    });
-});
